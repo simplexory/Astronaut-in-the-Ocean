7c9eccf600e8837d9cdab087e0e51823ae75a82d
Hankovich <simplexory@gmail.com>
2022-11-06 20:53:12 +0100

    add MainMenu outlets

--- AITO/Controller/MainMenuViewController.swift
+++ AITO/Controller/MainMenuViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
     
+    @IBOutlet weak var mainMenuImageView: UIImageView!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.mainMenuImageView.roundCorners(radius: 10)
+        self.mainMenuImageView.addBlackGradient()
+        self.startGameButton.dropShadow()
+    }
+    
     @IBAction func startGamePressed(_ sender: UIButton) {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else { return }
         controller.modalPresentationStyle = .fullScreen
