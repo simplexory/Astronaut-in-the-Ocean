27b9fec9e37fa1ea5095f9f4819b5f54cb884029
Hankovich <simplexory@gmail.com>
2022-11-06 19:35:56 +0100

    add player movement + jump

--- AITO/Model/AstronautModel.swift
+++ AITO/Model/AstronautModel.swift
@@ -13,6 +13,10 @@ private extension TimeInterval {
     static let animationDuration = 0.5
 }
 
+private extension Double {
+    static let sizeWhileJumping = 1.5
+}
+
 private extension String {
     static let modelName = "astronaut_"
     static let fileFormat = ".png"
@@ -27,6 +31,50 @@ final class Astronaut: UIImageView {
     
     private var imgAnimationSet: [UIImage] = []
     private var staticModel = UIImage()
+    var isJumpingNow = false
+    
+    func jump(duration: TimeInterval) {
+        let startSize = self.frame.size
+        self.isJumpingNow = true
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, options: .autoreverse) {
+            self.frame.size = CGSize(
+                width: self.frame.size.width * Double.sizeWhileJumping,
+                height: self.frame.size.height * Double.sizeWhileJumping
+            )
+        } completion: { _ in
+            self.frame.size = startSize
+            self.isJumpingNow = false
+        }
+    }
+    
+    func detectPlayerMove(direction: Move) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveLinear) {
+            
+            
+            switch direction {
+            case .left:
+                self.frame.origin.x -= self.frame.width / 3
+            case .right:
+                self.frame.origin.x += self.frame.width / 3
+            }
+        } completion: { isCancelled in
+            guard !isCancelled else { return }
+            
+            if let layer = self.layer.presentation() {
+                self.frame.origin.x = layer.frame.origin.x
+            }
+        }
+        
+    }
+    
+    func configurePlayerAnimation() {
+        self.layer.contentsGravity = .resize
+        self.layer.borderWidth = 2
+        self.animationImages = setImageAnimationSet()
+        self.animationDuration = .animationDuration
+        self.image = UIImage(named: .modelName + String(Int.startImageNumber) + .fileFormat)
+    }
     
     private func setImageAnimationSet() -> [UIImage] {
         var imgListArray: [UIImage] = []
@@ -41,14 +89,6 @@ final class Astronaut: UIImageView {
         }
         
         return imgListArray
-    }
-    
-    func addSelfAnimation() {
-        self.layer.contentsGravity = .resize
-        self.layer.borderWidth = 2
-        self.animationImages = setImageAnimationSet()
-        self.animationDuration = .animationDuration
-        self.image = UIImage(named: .modelName + String(Int.startImageNumber) + .fileFormat)
     }
     
 }
