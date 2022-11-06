29587177eb29405d4e17e51a834f32aa678be5e2
Hankovich <simplexory@gmail.com>
2022-11-06 19:35:26 +0100

    add apex model

---
+++ AITO/Model/Apex Model.swift
@@ -0,0 +1,39 @@
+//
+//  Apex Model.swift
+//  AITO
+//
+//  Created by Юра Ганкович on 6.11.22.
+//
+
+import Foundation
+import UIKit
+
+private extension String {
+    static let modelName = "stone"
+    static let fileFormat = ".png"
+}
+
+final class Apex: UIImageView {
+   
+    var inMovement = false
+    
+    func configureApex() {
+        self.layer.contentsGravity = .resize
+        self.layer.borderWidth = 2
+        self.image = UIImage(named: .modelName + .fileFormat)
+    }
+    
+    func startApexMovement(timeInterval: TimeInterval, multiplyer: Double, x: CGFloat, startY: CGFloat, endY: CGFloat) {
+        self.frame.origin.x = x
+        self.frame.origin.y = startY
+        self.inMovement = true
+        
+        UIView.animate(withDuration: timeInterval * multiplyer, delay: 0, options: .curveLinear) {
+            self.frame.origin.y = endY
+        } completion: { _ in
+            self.frame.origin.y = startY
+            self.inMovement = false
+        }
+    }
+    
+}
