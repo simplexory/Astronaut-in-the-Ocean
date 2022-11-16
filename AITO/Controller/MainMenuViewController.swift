76bf50cff1d2fb878d27f60dd134d033e4f772f3
Hankovich <simplexory@gmail.com>
2022-11-16 19:41:24 +0100

    add playerNameLabel + label for scores

--- AITO/Controller/MainMenuViewController.swift
+++ AITO/Controller/MainMenuViewController.swift
@@ -4,17 +4,37 @@ class MainMenuViewController: UIViewController {
     
     @IBOutlet weak var mainMenuImageView: UIImageView!
     @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var playerNameTextField: UITextField!
+    @IBOutlet weak var scoreTableLabel: UILabel!
+    
+    private var match: PlayerMatch?
+    
+    override func viewDidLoad() {
+        guard let lastMatch = StorageManager.shared.loadMatch() else { return }
+        self.match = lastMatch
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         self.mainMenuImageView.roundCorners(radius: 10)
         self.mainMenuImageView.addBlackGradient()
         self.startGameButton.dropShadow()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        let playerName = StorageManager.shared.loadName()
+        self.playerNameTextField.text = playerName
+    }
+    
     @IBAction func startGamePressed(_ sender: UIButton) {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else { return }
         controller.modalPresentationStyle = .fullScreen
+        
+        if let playerName = self.playerNameTextField.text {
+            StorageManager.shared.saveName(playerName)
+        }
+        
         
         self.navigationController?.pushViewController(controller, animated: false)
     }
--- AITO/Helpers/StorageManager.swift
+++ AITO/Helpers/StorageManager.swift
@@ -1,8 +1,30 @@
-//
-//  StorageManager.swift
-//  AITO
-//
-//  Created by Юра Ганкович on 16.11.22.
-//
-
 import Foundation
+
+private extension String {
+    static let playerNameKey = "playerNameKey"
+    static let matchKey = "matchKey"
+}
+
+class StorageManager {
+    static let shared = StorageManager()
+    private init() {}
+    
+    func saveName(_ name: String) {
+        UserDefaults.standard.set(name, forKey: .playerNameKey)
+    }
+    
+    func loadName() -> String {
+        guard let name = UserDefaults.standard.object(forKey: .playerNameKey) as? String else { return "Player" }
+        return name
+    }
+    
+    func saveMatch(_ playerMatches: PlayerMatch) {
+        UserDefaults.standard.set(playerMatches, forKey: .matchKey)
+    }
+    
+    func loadMatch() -> PlayerMatch? {
+        guard let match = UserDefaults.standard.object(forKey: .matchKey) as? PlayerMatch else { return nil }
+        return match
+    }
+    
+}
