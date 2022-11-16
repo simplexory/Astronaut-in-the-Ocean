7ec663a5e82247c8012bd0f39a12235fd7116733
Hankovich <simplexory@gmail.com>
2022-11-16 19:40:24 +0100

    add storageManager + PlayerMatch struct

---
+++ AITO/Helpers/PlayerMatch.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct PlayerMatch {
+    let name: String
+    let score: Int
+}
---
+++ AITO/Helpers/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  AITO
+//
+//  Created by Юра Ганкович on 16.11.22.
+//
+
+import Foundation
