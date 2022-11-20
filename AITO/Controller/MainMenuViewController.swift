2be857215708d708f557a960839aacea237c76c4
Hankovich <simplexory@gmail.com>
2022-11-20 13:52:15 +0100

    update mainMenuViewController class + refactoring

--- AITO/Controller/MainMenuViewController.swift
+++ AITO/Controller/MainMenuViewController.swift
@@ -22,7 +22,7 @@ private extension TimeInterval {
     static let buttonAnimationDuration: TimeInterval = 1.2
 }
 
-final class MainMenuViewController: UIViewController {
+class MainMenuViewController: UIViewController {
     
     @IBOutlet weak var mainMenuImageView: UIImageView!
     @IBOutlet weak var startGameButton: UIButton!
@@ -35,22 +35,22 @@ final class MainMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.buttonBottomConstraint.constant = .startButtonConstraint
+        buttonBottomConstraint.constant = .startButtonConstraint
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.startGameButton.setCyberpnukFont(text: .buttonText, size: .buttonFontSize)
-        self.playerNameTextField.setCyberthroneFont(size: .playerNameFontSize)
-        self.mainMenuImageView.roundCorners(radius: .roundCornerImageRadius)
-        self.mainMenuImageView.addBlackGradient()
+        startGameButton.setCyberpnukFont(text: .buttonText, size: .buttonFontSize)
+        playerNameTextField.setCyberthroneFont(size: .playerNameFontSize)
+        mainMenuImageView.roundCorners(radius: .roundCornerImageRadius)
+        mainMenuImageView.addBlackGradient()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let playerName = StorageManager.shared.loadName()
         
-        self.playerNameTextField.text = playerName
+        playerNameTextField.text = playerName
         guard isAnimated else { return animateButton() }
     }
     
@@ -61,29 +61,29 @@ final class MainMenuViewController: UIViewController {
             score = String(lastScore)
         }
         
-        self.scoreTableLabel.setCyberverseFont(text: "\(String.scoreText)\n\(score)", size: .labelFontSize)
+        scoreTableLabel.setCyberverseFont(text: "\(String.scoreText)\n\(score)", size: .labelFontSize)
     }
     
     private func animateButton() {
-        self.buttonBottomConstraint.constant = .buttonConstraint
-        self.startGameButton.layer.opacity = 0
-        self.playerInfoView.layer.opacity = 0
+        buttonBottomConstraint.constant = .buttonConstraint
+        startGameButton.layer.opacity = 0
+        playerInfoView.layer.opacity = 0
         UIView.animate(withDuration: .buttonAnimationDuration, delay: 0, options: .curveEaseOut) {
             self.startGameButton.layer.opacity = .viewOpacity
             self.playerInfoView.layer.opacity = .viewOpacity
             self.view.layoutIfNeeded()
         }
-        self.isAnimated = true
+        isAnimated = true
     }
     
     @IBAction func startGamePressed(_ sender: UIButton) {
-        guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else { return }
+        guard let controller = storyboard?.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController else { return }
         controller.modalPresentationStyle = .fullScreen
         
-        if let playerName = self.playerNameTextField.text {
+        if let playerName = playerNameTextField.text {
             StorageManager.shared.saveName(playerName)
         }
     
-        self.navigationController?.pushViewController(controller, animated: false)
+        navigationController?.pushViewController(controller, animated: false)
     }
 }
