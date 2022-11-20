be72bd3f9c8272dc318144cd9b783854aaa167a3
Hankovich <simplexory@gmail.com>
2022-11-20 13:51:54 +0100

    update settings

--- AITO/Helpers/Settings.swift
+++ AITO/Helpers/Settings.swift
@@ -1,24 +1,24 @@
 import UIKit
 
-public extension String {
+extension String {
     static let fileFormat = ".png"
 }
 
-public extension CGFloat {
+extension CGFloat {
     static let contentDivider: CGFloat = 6 // divide screen.width for models
     static let maxContentDivider: CGFloat = 10 // maximum divide screen.width for random apex size
     static let paddingBottomPlayerMultiplyer: CGFloat = 2.8 // bottom player padding = x * model.height
     static let playerMovementDivider: CGFloat = 3.2 // movement = model.width / x
 }
 
-public extension Double {
+extension Double {
     static let defaultSpeedMultiplyer = 1.0 // start speed
     static let speedMultiplyer = 0.95 // multiply speed by take() boost in game NOTE: < 1
     static let sizeWhileJumping = 1.5 // player model size while jumping
     static let rotatePlayerDegree = 25.0 // player rotating degree if move left/right
 }
 
-public extension Int {
+extension Int {
     // counter for objects
     static let apexObjectsCount = 8
     static let boostObjectsCount = 2
@@ -30,7 +30,7 @@ public extension Int {
     static let scorePerCoin = 5 // score per coin in game
 }
 
-public extension TimeInterval {
+extension TimeInterval {
     static let frameRate: TimeInterval = 1 / 60 // framerate
     //  spawnrates
     static let apexSpawnRate: TimeInterval = 3.2
@@ -39,5 +39,6 @@ public extension TimeInterval {
     static let boostSpawnRate: TimeInterval = 10
     // -- // --
     static let movementTime: TimeInterval = 5.8 // base movement time for objects
+    static let backgroundMovementTime: TimeInterval = 22.5 // base movement time for background
     static let defaultJumpDuration: TimeInterval = 1.15  // player jump duration
 }
