f5ee37f9f1f388fe0e32d12c5748d2119f0e6728
Hankovich <simplexory@gmail.com>
2022-11-21 21:52:07 +0100

    update assets + settings for models

--- AITO/Helpers/Settings.swift
+++ AITO/Helpers/Settings.swift
@@ -1,16 +1,19 @@
 import UIKit
 
 extension CGFloat {
-    static let contentDivider: CGFloat = 6 // divide screen.width for models
-    static let maxContentDivider: CGFloat = 10 // maximum divide screen.width for random apex size
+    static let playerContentDivider: CGFloat = 6 // divide view.width for player size
+    static let boosterContentDivider: CGFloat = 7.1 // divide view.width for booster size
+    static let minApexContentDivider: CGFloat = 6.3 // divide view.width for apex size
+    static let maxApexContentDivider: CGFloat = 8 // > minApexContentDivider
+    static let coinContentDivider: CGFloat = 10.5 // divide view.width for coin size
     static let playerPaddingMultiplyer: CGFloat = 7.5 // bottom player padding = view / x
-    static let playerMovementDivider: CGFloat = 3.2 // movement = model.width / x
+    static let playerMovementDivider: CGFloat = 1.6 // movement = model.width / x
 }
 
 extension Double {
     static let defaultSpeedMultiplyer = 1.0 // start speed
     static let speedMultiplyer = 0.95 // multiply speed by take() boost in game NOTE: < 1
-    static let sizeWhileJumping = 1.5 // player model size while jumping
+    static let sizeWhileJumping = 2.5 // player model size while jumping
     static let rotatePlayerDegree = 25.0 // player rotating degree if move left/right
 }
 
@@ -29,10 +32,10 @@ extension Int {
 extension TimeInterval {
     static let frameRate: TimeInterval = 1 / 60 // framerate
     //  spawnrates
-    static let apexSpawnRate: TimeInterval = 3.2
-    static let jumpBoardSpawnRate: TimeInterval = 6.5
-    static let coinsSpawnRate: TimeInterval = 1.5
-    static let boostSpawnRate: TimeInterval = 10
+    static let apexSpawnRate: TimeInterval = 3.25
+    static let jumpBoardSpawnRate: TimeInterval = 6.55
+    static let coinsSpawnRate: TimeInterval = 1.53
+    static let boostSpawnRate: TimeInterval = 10.1
     // -- // --
     static let movementTime: TimeInterval = 5.8 // base movement time for objects
     static let backgroundMovementTime: TimeInterval = 22.5 // base movement time for background
