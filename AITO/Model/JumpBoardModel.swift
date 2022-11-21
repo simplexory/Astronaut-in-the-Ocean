57eaf624879dba909b6857fb027a8f6064ad13f5
Hankovich <simplexory@gmail.com>
2022-11-21 21:53:29 +0100

    remove border.width from models

--- AITO/Model/JumpBoardModel.swift
+++ AITO/Model/JumpBoardModel.swift
@@ -8,7 +8,6 @@ final class JumpBoard: GameObject {
    
     override func setup() {
         self.model.layer.contentsGravity = .resize
-        self.model.layer.borderWidth = 2
         self.model.image = UIImage(named: .modelName)
     }
     
